ca9065764e419727785ccd923aaa9e3b35ce9971
zer0ttl <sudheer.shirsath+zer0ttl@gmail.com>
2018-04-11 19:47:54 +0530

    signup completed.

--- feelings/users/views.py
+++ feelings/users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth import logout
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 from django.http import HttpResponseRedirect
 from django.views.generic import FormView, CreateView
 from django.contrib.auth.forms import UserCreationForm
@@ -25,3 +25,5 @@ class SignupView(CreateView):
     form_class = UserCreationForm
     template_name = 'registration/signup.html'
 
+    def get_success_url(self):
+        return reverse_lazy('users:login')
